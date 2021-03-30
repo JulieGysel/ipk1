b0898df58cae1d556c41c76b7ba6331a26d7248a
JulieGysel <julie.gysel@gmail.com>
2021-03-30 17:52:58 +0200

    wip

--- test.sh
+++ test.sh
@@ -6,3 +6,16 @@ do
     diff "$file" nameserver/file.server.one/"$file" && echo "$file identical" || echo "$file NOT identical"
     rm "$file"
 done
+
+echo ""
+echo "TESTING GETALL"
+echo ""
+./fileget -n 127.0.0.2:42069 -f fsp://file.server.one/*
+
+for file in $(find nameserver/file.server.one -type f | cut -d'/' -f 3,4)
+do
+    diff "$file" nameserver/file.server.one/"$file" && echo "$file identical" || echo "$file NOT identical"
+    rm "$file"
+done
+
+rm -r folder
